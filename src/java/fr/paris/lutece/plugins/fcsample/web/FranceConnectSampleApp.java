35cd62485f7fa1d555cfd1f6753f8a869c77d7da
pierrelevy <pierrelevy@users.noreply.github.com>
2015-06-14 19:26:49 +0200

    Initialisation - Work in progress

--- src/java/fr/paris/lutece/plugins/fcsample/web/FranceConnectSampleApp.java
+++ src/java/fr/paris/lutece/plugins/fcsample/web/FranceConnectSampleApp.java
@@ -33,65 +33,149 @@
  */
 package fr.paris.lutece.plugins.fcsample.web;
 
-import fr.paris.lutece.portal.service.security.LuteceUser;
-import fr.paris.lutece.portal.service.security.SecurityService;
-import fr.paris.lutece.portal.service.security.UserNotSignedException;
-import fr.paris.lutece.portal.web.xpages.XPage;
-import fr.paris.lutece.portal.util.mvc.xpage.MVCApplication;
+import fr.paris.lutece.plugins.fcsample.business.FormData;
+import fr.paris.lutece.plugins.fcsample.dataclient.UserDataClient;
+import fr.paris.lutece.plugins.fcsample.service.RedirectUtils;
+import fr.paris.lutece.plugins.franceconnect.oidc.UserInfo;
+import fr.paris.lutece.plugins.franceconnect.service.DataClientService;
+import fr.paris.lutece.portal.util.mvc.commons.annotations.Action;
 import fr.paris.lutece.portal.util.mvc.commons.annotations.View;
+import fr.paris.lutece.portal.util.mvc.xpage.MVCApplication;
 import fr.paris.lutece.portal.util.mvc.xpage.annotations.Controller;
+import fr.paris.lutece.portal.web.xpages.XPage;
+
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 /**
  * This class provides a simple implementation of an XPage
  */
- 
-@Controller( xpageName = "fcsample" , pageTitleI18nKey = "fcsample.xpage.fcsample.pageTitle" , pagePathI18nKey = "fcsample.xpage.fcsample.pagePathLabel" )
+@Controller( xpageName = "fcsample", pageTitleI18nKey = "fcsample.xpage.fcsample.pageTitle", pagePathI18nKey = "fcsample.xpage.fcsample.pagePathLabel" )
 public class FranceConnectSampleApp extends MVCApplication
 {
+    // VIEWS
+    public static final String VIEW_HOME = "home";
+    public static final String VIEW_DEMARCHE_FORM = "demarcheForm";
+    public static final String VIEW_DEMARCHE_ETAPE2 = "demarcheEtape2";
+    public static final String VIEW_DEMARCHE_FIN = "demarcheFin";
+
+    // ACTIONS
+    public static final String ACTION_START_DEMARCHE = "startDemarche";
+    public static final String ACTION_PROCESS_FORM = "processForm";
+
+    // TEMPLATES
     private static final String TEMPLATE_XPAGE = "/skin/plugins/fcsample/home.html";
     private static final String TEMPLATE_DEMARCHE_FORM = "/skin/plugins/fcsample/demarche-form.html";
-    private static final String VIEW_HOME = "home";
-    private static final String VIEW_DEMARCHE_FORM = "demarcheForm";
+    private static final String TEMPLATE_DEMARCHE_ETAPE2 = "/skin/plugins/fcsample/demarche-etape2.html";
+    private static final String TEMPLATE_DEMARCHE_FIN = "/skin/plugins/fcsample/demarche-etape-fin.html";
     private static final String MARK_LASTNAME = "lastname";
     private static final String MARK_FIRSTNAME = "firstname";
-    
+    private static final String MARK_FORM_DATA = "form";
+    private static final String DATACLIENT_USER = "user";
+    private static final String DATACLIENT_REVENU = "revenu";
     private static final long serialVersionUID = 1L;
-    
+    private UserInfo _userInfo;
+    private FormData _formData;
+
     /**
-     * Returns the content of the page fcsample. 
+     * Returns the content of the page fcsample.
      * @param request The HTTP request
      * @return The view
      */
-    @View( value = VIEW_HOME , defaultView = true )
+    @View( value = VIEW_HOME, defaultView = true )
     public XPage viewHome( HttpServletRequest request )
     {
         return getXPage( TEMPLATE_XPAGE, request.getLocale(  ) );
     }
-    
+
     /**
-     * Returns the content of the page fcsample. 
+     * Start the process by connecting to FranceConnect
+     * @param request The HTTP request
+     * @return Redirection
+     */
+    @Action( ACTION_START_DEMARCHE )
+    public XPage startDemarche( HttpServletRequest request )
+    {
+        _userInfo = (UserInfo) request.getSession(  ).getAttribute( UserDataClient.ATTRIBUTE_USERINFO );
+
+        if ( _userInfo == null )
+        {
+            String strUrl = DataClientService.instance(  ).getDataClientUrl( DATACLIENT_USER );
+
+            return redirect( request, strUrl );
+        }
+
+        return redirectView( request, VIEW_DEMARCHE_FORM );
+    }
+
+    /**
+     * Returns the content of the page fcsample.
      * @param request The HTTP request
      * @return The view
      */
-    @View( value = VIEW_DEMARCHE_FORM )
-    public XPage viewDemarcheForm( HttpServletRequest request ) throws UserNotSignedException
+    @View( VIEW_DEMARCHE_FORM )
+    public XPage viewDemarcheForm( HttpServletRequest request )
     {
-        LuteceUser user = SecurityService.getInstance().getRegisteredUser(request);
-        
-        if( user == null )
+        _userInfo = (UserInfo) request.getSession(  ).getAttribute( UserDataClient.ATTRIBUTE_USERINFO );
+
+        if ( _userInfo == null )
         {
-            throw new UserNotSignedException();
+            return redirect( request, RedirectUtils.getActionUrl( request, ACTION_START_DEMARCHE ) );
         }
-        
-        Map<String, Object> model = getModel();
-        model.put( MARK_FIRSTNAME , user.getUserInfo( LuteceUser.NAME_GIVEN ) );
-        model.put( MARK_LASTNAME , user.getUserInfo( LuteceUser.NAME_FAMILY ) );
-        return getXPage( TEMPLATE_DEMARCHE_FORM, request.getLocale(  ) , model );
+
+        Map<String, Object> model = getModel(  );
+        model.put( MARK_FIRSTNAME, _userInfo.getGivenName(  ) );
+        model.put( MARK_LASTNAME, _userInfo.getFamilyName(  ) );
+
+        return getXPage( TEMPLATE_DEMARCHE_FORM, request.getLocale(  ), model );
     }
-    
-    
-    
+
+    /**
+     * Process the form
+     * @param request The HTTP request
+     * @return Redirection
+     */
+    @Action( ACTION_PROCESS_FORM )
+    public XPage processForm( HttpServletRequest request )
+    {
+        _formData = new FormData(  );
+        populate( _formData, request );
+
+        if ( !validateBean( _formData ) )
+        {
+            // TODO Process error
+        }
+
+        System.out.println( "Données du formulaire : " + _formData.getPrenomEnfant(  ) + " " +
+            _formData.getNomEnfant(  ) );
+
+        return redirect( request, DataClientService.instance(  ).getDataClientUrl( DATACLIENT_REVENU ) );
+    }
+
+    /**
+     * Display the view Etape2
+     * @param request The HTTP request
+     * @return The XPage
+     */
+    @View( VIEW_DEMARCHE_ETAPE2 )
+    public XPage viewDemarcheEtape2( HttpServletRequest request )
+    {
+        Map<String, Object> model = getModel(  );
+        model.put( MARK_FORM_DATA, _formData );
+
+        return getXPage( TEMPLATE_DEMARCHE_ETAPE2, request.getLocale(  ), model );
+    }
+
+    /**
+     * Display the view Etape FIN
+     * @param request The HTTP request
+     * @return The XPage
+     */
+    @View( VIEW_DEMARCHE_FIN )
+    public XPage viewDemarcheFin( HttpServletRequest request )
+    {
+        return getXPage( TEMPLATE_DEMARCHE_FIN, request.getLocale(  ) );
+    }
 }
